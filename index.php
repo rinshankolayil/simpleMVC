b0778834bfbc13a04d6b4033e212ba7e09fc7981
Rinshan Kolayil <40558052+rinshankolayil@users.noreply.github.com>
2021-05-05 14:48:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
 session_start();
-$test_user = array(
-	'rinshan',
-);
-if(isset($_SESSION['UserID'])){
-	if(!in_array($_SESSION['UserID'],$test_user)){
-		ini_set('display_errors', '1');
-	}
-	else{
-		ini_set('display_errors', '0');
-	}	
-}
 ini_set('display_errors', '1');
 include('app.php');
 $class = new App();
