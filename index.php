609fdd397b32e5316dcdd3ddabd6842011590853
Rinshan Kolayil <40558052+rinshankolayil@users.noreply.github.com>
2021-05-05 14:49:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-ini_set('display_errors', '1');
+ini_set('display_errors', '1'); ## DEBUG = True in Django framework
 include('app.php');
 $class = new App();
 //if you want to learn how to creat MVC structure in PHP Please refer youtube
